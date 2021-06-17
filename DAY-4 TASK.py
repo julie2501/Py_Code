9910a2894af87aaa6f9959145493039cce4a0831
julie2501 <61940879+julie2501@users.noreply.github.com>
2021-06-17 18:22:57 +0530

    Best_Enlist_Python_internship

---
+++ DAY-4 TASK.py
@@ -0,0 +1,26 @@
+# create three variables with same value
+
+a = b = c = 20
+
+# divide a by 10
+divi = a/5
+print(int(divi))
+
+# multiply b by 50
+mul = b * 30
+print(mul)
+
+# add c value by 60
+add_1 = c + 50
+print(add_1)
+
+# create string of 5 charcters and replace 3rd character with G
+var_1 = "aeiou"
+x = var_1.replace(var_1[2],"G")
+print(x)
+
+# create two values of int float datatypes and covert them viceversa
+a,b = 17,25.8
+a1 = (float(a))
+b1 = (int(b))
+print(a1,b1)
